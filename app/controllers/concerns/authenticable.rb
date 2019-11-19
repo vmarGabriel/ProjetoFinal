da637886083ce1d04d065364110dfea9ac19cdbf
andre-tlima <andre-tlima@hotmail.com>
2019-11-19 21:51:01 +0000

    Configurando autenticação e restrições

---
+++ app/controllers/concerns/authenticable.rb
@@ -0,0 +1,10 @@
+module Authenticable
+    
+    def current_user
+        @current_user ||= User.find_by(auth_token: request.headers["Authorization"])
+    end
+    
+    def authenticate_with_token!
+        render json: { errors: "Acesso não autorizado!"}, status: 401 unless current_user.present?
+    
+end
