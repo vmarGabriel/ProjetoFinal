acf4975c662f60c1ce522cd69643843799ed5a31
andre-tlima <andre-tlima@hotmail.com>
2019-11-21 14:40:31 +0000

    Adicionando ajustes finais na API

--- app/serializers/api/v2/expenses_serializer.rb
+++ app/serializers/api/v2/expenses_serializer.rb
@@ -5,5 +5,9 @@ class Api::V2::ExpensesSerializer < ActiveModel::Serializer
         Date.current > object.date if object.date.present?
     end
     
+    def date_to_br
+        I18n.l(object.date, format: :databr) if object.date.present?
+    end
+    
     belongs_to :user
 end
--- app/serializers/api/v2/gain_serializer.rb
+++ app/serializers/api/v2/gain_serializer.rb
@@ -5,5 +5,10 @@ class Api::V2::GainSerializer < ActiveModel::Serializer
         Date.current > object.date if object.date.present?
     end
     
+    def date_to_br
+        I18n.l(object.date, format: :databr) if object.date.present?
+    end
+    
+    
     belongs_to :user
 end
